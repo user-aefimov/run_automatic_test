dbc48e3781cbcc93a1f2112d56994b2f6bb955c0
user-aefimov <user.a.efimov@gmail.com>
2026-02-02 12:05:29 +0300

    corrections imports in test_button.py

--- tests/test_button.py
+++ tests/test_button.py
@@ -1,15 +1,34 @@
-from selenium import webdriver
-from selenium.webdriver.common.by import By
-from selenium.webdriver.chrome.options import Options
-import pytest
+# from selenium import webdriver
+# from selenium.webdriver.common.by import By
+# from selenium.webdriver.chrome.options import Options
+# import pytest
 
+
+# @pytest.fixture()
+# def browser():
+#     options = Options()
+#     options.add_argument('--headless')
+#     chrome_browser = webdriver.Chrome(options=options)
+#     return chrome_browser
+
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.chrome.service import Service
+from webdriver_manager.chrome import ChromeDriverManager
+import pytest
 
 @pytest.fixture()
 def browser():
     options = Options()
     options.add_argument('--headless')
-    chrome_browser = webdriver.Chrome(options=options)
-    return chrome_browser
+    
+    # Автоматическое скачивание и использование правильного ChromeDriver
+    service = Service(ChromeDriverManager().install())
+    chrome_browser = webdriver.Chrome(service=service, options=options)
+    
+    yield chrome_browser
+    chrome_browser.quit()
+
 
 
 def test_button_exist(browser):
