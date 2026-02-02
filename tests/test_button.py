ab5f246493e644b4945ccffef423dbeddfb522bb
user-aefimov <user.a.efimov@gmail.com>
2026-02-02 14:08:55 +0300

    second

--- tests/test_button.py
+++ tests/test_button.py
@@ -14,6 +14,7 @@
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.common.by import By  # <-- ДОБАВИТЬ ЭТУ СТРОКУ
 from webdriver_manager.chrome import ChromeDriverManager
 import pytest
 
